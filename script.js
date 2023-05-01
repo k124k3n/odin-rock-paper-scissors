43a0c8a566e0e11061b87ceecc92c2cc948e276b
k124k3n <d1041221003@student.untan.ac.id>
2023-05-01 11:56:25 +0700

    Add playRound function

--- script.js
+++ script.js
@@ -1,7 +1,26 @@
-function getComputerChoice(){
-    let choiceList = [`Rock`, `Paper`, `Scissors`];
+let choiceList = [`Rock`, `Paper`, `Scissors`];
 
 function getComputerChoice(){
     let randomNum = Math.floor(Math.random() * 3);
     return choiceList[randomNum];
+}
+
+function playRound(playerSelection, computerSelection){
+    playerSelection = playerSelection[0].toUpperCase() + playerSelection.substr(1).toLowerCase();
+    let playerIndex = choiceList.indexOf(playerSelection);
+    let computerIndex = choiceList.indexOf(computerSelection);
+    let result;
+    if((playerIndex+1)%3 === computerIndex){
+        result = "You Lose! " + computerSelection + " beats " + playerSelection;
+    }
+
+    else if(playerIndex === computerIndex){
+        result = "Draw!";
+    }
+
+    else{
+        result = "You Win!" + playerSelection + " beats " + computerSelection;
+    }
+
+    return result;
 }

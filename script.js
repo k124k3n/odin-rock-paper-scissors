dfd46b358b6119b5cedf6480c53e6d573666c739
k124k3n <d1041221003@student.untan.ac.id>
2023-05-01 11:55:59 +0700

    Move choiceList to global scope

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 function getComputerChoice(){
     let choiceList = [`Rock`, `Paper`, `Scissors`];
+
+function getComputerChoice(){
     let randomNum = Math.floor(Math.random() * 3);
     return choiceList[randomNum];
 }
